0290a5af246f398241b85ba3fa08234b0453bea4
haiverg <haiver.galeano@gmail.com>
2016-09-21 21:08:41 -0500

    last minute adjust

--- hgp-bean-registry/src/main/java/com/redhat/training/TaxCalculator.java
+++ hgp-bean-registry/src/main/java/com/redhat/training/TaxCalculator.java
@@ -2,7 +2,7 @@ package com.redhat.training;
 
 public class TaxCalculator {
 
-	public String processTax(String data) {
+	public String processTax(String data) throws Exception{
 		String[] columns = data.split(",");
 		double value = Double.parseDouble(columns[3]);
 		double result = (value*7.5)/100;
